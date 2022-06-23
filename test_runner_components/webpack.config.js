283cc60f4c6f09e0ce21a81be8e2970836cefc61
Ivan <ichang@broadinstitute.org>
2022-06-23 17:34:59 -0400

    Update webpack.config.

--- test_runner_components/webpack.config.js
+++ test_runner_components/webpack.config.js
@@ -63,9 +63,14 @@ module.exports = (env, argv) => {
                 {
                     test: /\.(js|jsx)$/,
                     exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                    },
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                        },
+                        {
+                            loader: 'ify-loader',
+                        },
+                    ],
                 },
                 {
                     test: /\.s[ac]ss$/i,
