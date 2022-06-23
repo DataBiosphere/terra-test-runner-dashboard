443873b5c8e6377070160fd42bea639855825402
Ivan <ichang@broadinstitute.org>
2022-06-23 17:31:32 -0400

    React components for rendering dashboard tables.

---
+++ test_runner_components/src/lib/components/Tables/IntegrationTestsTable.jsx
@@ -0,0 +1,126 @@
+import MaterialTable from "@material-table/core";
+import React, {Component} from 'react';
+import Plot from 'react-plotly.js';
+import PropTypes from "prop-types";
+
+const cols = [
+    {title: 'Start time', field: 'startUserJourneyTimestamp', type: 'datetime'},
+    {title: 'Name', field: 'testScriptName'},
+    {title: 'Total', field: 'totalRun', type: 'numeric', hidden: true},
+    {title: 'Trend date', field: 'trend_date', hidden: true},
+    {title: 'Trend totalRun', field: 'trend_totalRun', hidden: true},
+    {title: 'Trend numCompleted', field: 'trend_numCompleted', hidden: true},
+    {title: 'Trend numExceptionsThrown', field: 'trend_numExceptionsThrown', hidden: true},
+    {title: 'Trend numExceptionsThrown neg', field: 'trend_numExceptionsThrown_neg', hidden: true},
+    {title: 'Outcome trend', field: 'trend',
+        render: (row) =>
+            <Plot
+                config={
+                    {
+                        displayModeBar: false,
+                    }
+                }
+                data={
+                    [
+                        {
+                            x: row.trend_date,
+                            y: row.trend_numCompleted,
+                            name: 'Passed',
+                            type: 'bar',
+                            width: 0.3,
+                            hovertemplate: '<i>%{x} Passed</i>: %{y}',
+                            marker: {
+                                color: 'rgba(50, 171, 96, 0.7)',
+                            },
+                        },
+                        {
+                            x: row.trend_date,
+                            y: row.trend_numExceptionsThrown_neg,
+                            text: row.trend_numExceptionsThrown,
+                            name: 'Failed',
+                            type: 'bar',
+                            width: 0.3,
+                            hovertemplate: '<i>%{x} Failed</i>: %{text}',
+                            marker: {
+                                color: 'rgba(219, 64, 82, 0.7)',
+                            }
+                        }
+                    ]
+                }
+                layout={ {
+                    width: 200,
+                    height: 120,
+                    barmode: 'relative',
+                    showlegend: false,
+                    margin: {
+                        l: 50,
+                        r: 50,
+                        b: 50,
+                        t: 50,
+                        pad: 4
+                    },
+                    xaxis: {
+                        visible: false,
+                        showticklabels: false,
+                    },
+                    yaxis: {
+                        visible: false,
+                        showticklabels: false,
+                    },
+                } }
+            />
+    },
+    {title: 'Failed', field: 'numExceptionsThrown', type: 'numeric'},
+    {title: 'Passed', field: 'numCompleted', type: 'numeric'},
+    {title: 'Skipped', field: 'numSkipped', type: 'numeric',
+        render: (row) =>
+            row.totalRun - row.numCompleted - row.numExceptionsThrown
+    },
+];
+
+class IntegrationTestsTable extends Component {
+    render() {
+        const {data, title, barWidth} = this.props;
+        return (
+            <div>
+                <MaterialTable
+                    columns={cols}
+                    data={data}
+                    title={title}
+                    options={{
+                        cellStyle: {
+                            fontFamily: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                            fontSize: "12px",
+                        },
+                        headerStyle: {
+                            fontFamily: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                        }
+                    }}
+                />
+            </div>
+        );
+    }
+}
+
+IntegrationTestsTable.defaultProps = {};
+
+IntegrationTestsTable.propTypes = {
+    /**
+     * The ID used to identify this component in Dash callbacks.
+     */
+    id: PropTypes.string,
+    /**
+     * The data rendered by the table.
+     */
+    data: PropTypes.array,
+    /**
+     * The title of the table.
+     */
+    title: PropTypes.string,
+    /**
+     * The bar width of the Bar Charts.
+     */
+    barWidth: PropTypes.number,
+};
+
+export default IntegrationTestsTable;
---
+++ test_runner_components/src/lib/components/Tables/PerfTestsTable.jsx
@@ -0,0 +1,166 @@
+import MaterialTable from "@material-table/core";
+import React, {Component} from 'react';
+import Plot from 'react-plotly.js';
+import PropTypes from "prop-types";
+
+const cols = [
+    {title: 'Test time', field: 'startUserJourneyTimestamp', type: 'datetime'},
+    {title: 'Name', field: 'testScriptName', type: 'string'},
+    {title: 'Trend date', field: 'trend_date', hidden: true},
+    {title: 'Trend mean', field: 'trend_mean', hidden: true},
+    {title: 'Trend p50', field: 'trend_p50', hidden: true},
+    {title: 'Trend p95', field: 'trend_p95', hidden: true},
+    {title: 'Trend min', field: 'trend_min', hidden: true},
+    {title: 'Trend max', field: 'trend_max', hidden: true},
+    {title: 'Outcome trend', field: 'perf_trend',
+        render: (row) =>
+            <div><Plot
+                config={
+                    {
+                        displayModeBar: false,
+                    }
+                }
+                data={
+                    [
+                        {
+                            x: row.trend_date,
+                            y: row.trend_min,
+                            name: 'min [ms]',
+                            type: 'scatter',
+                            mode: 'lines',
+                            line: {
+                                width: 0,
+                            },
+                            showlegend: false,
+                        },
+                        {
+                            x: row.trend_date,
+                            y: row.trend_mean,
+                            name: 'mean [ms]',
+                            type: 'scatter',
+                            mode: 'lines',
+                            line: {
+                                color: 'rgba(63, 176, 198, 255)',
+                                dash: 'dash',
+                            },
+                            fill: 'tonexty',
+                            fillcolor: 'rgba(170, 218, 277, 255)',
+                            showlegend: false,
+                        },
+                        {
+                            x: row.trend_date,
+                            y: row.trend_p95,
+                            name: 'p95 [ms]',
+                            type: 'scatter',
+                            mode: 'lines',
+                            line: {
+                                width: 0,
+                            },
+                            fill: 'tonexty',
+                            fillcolor: 'rgba(130, 200, 213, 255)',
+                            showlegend: false,
+                        },
+                        {
+                            x: row.trend_date,
+                            y: row.trend_max,
+                            name: 'max [ms]',
+                            type: 'scatter',
+                            mode: 'lines',
+                            line: {
+                                width: 0,
+                            },
+                            fill: 'tonexty',
+                            fillcolor: 'rgba(170, 218, 277, 255)',
+                            showlegend: false,
+                        },
+                        {
+                            x: row.trend_date,
+                            y: row.trend_p50,
+                            name: 'p50 [ms]',
+                            type: 'scatter',
+                            mode: 'lines',
+                            line: {
+                                color: 'rgba(67, 178, 199, 255)',
+                            },
+                            showlegend: false,
+                        },
+                    ]
+                }
+                layout={ {
+                    width: 320,
+                    height: 240,
+                    legend: {
+                        font: {
+                            family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                            size: "9px",
+                        },
+                        yanchor: 'top',
+                        y: 1.01,
+                        xanchor: 'left',
+                        x: 0.1,
+                    },
+                    xaxis: {
+                        tickfont: {
+                            family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                            size: "9px",
+                        },
+                    },
+                    yaxis: {
+                        tickfont: {
+                            family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                            size: "9px",
+                        },
+                    },
+                } }
+            /></div>
+    },
+    {title: 'Min [ms]', field: 'min', type: 'numeric'},
+    {title: 'Max [ms]', field: 'max', type: 'numeric'},
+    {title: 'Mean [ms]', field: 'mean', type: 'numeric'},
+    {title: 'Standard deviation [ms]', field: 'sd', type: 'numeric'},
+    {title: 'p50 [ms]', field: 'p50', type: 'numeric'},
+    {title: 'p95 [ms]', field: 'p95', type: 'numeric'},
+];
+
+class PerfTestsTable extends Component {
+    render() {
+        const {data, title} = this.props;
+        return (
+            <div>
+                <MaterialTable
+                    columns={cols}
+                    data={data}
+                    title={title}
+                    options={{
+                        cellStyle: {
+                            fontFamily: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                            fontSize: "12px",
+                        },
+                        headerStyle: {
+                            fontFamily: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                        }
+                    }}
+                />
+            </div>
+        );
+    }
+}
+
+PerfTestsTable.defaultProps = {};
+
+PerfTestsTable.propTypes = {
+    /**
+     * The ID used to identify this component in Dash callbacks.
+     */
+    id: PropTypes.string,
+    /**
+     * The data rendered by the table.
+     */
+    data: PropTypes.array,
+    /**
+     * The title of the table.
+     */
+    title: PropTypes.string
+};
+
+export default PerfTestsTable;
