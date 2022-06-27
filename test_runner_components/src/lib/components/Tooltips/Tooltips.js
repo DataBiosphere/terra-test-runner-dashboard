3f4c2b00b7087753b9b0d6ee5543f1c2fb0f34a2
Ivan <ichang@broadinstitute.org>
2022-06-27 14:57:15 -0400

    Restyle tooltip.

--- test_runner_components/src/lib/components/Tooltips/Tooltips.js
+++ test_runner_components/src/lib/components/Tooltips/Tooltips.js
@@ -5,14 +5,6 @@ import React, {Component} from 'react';
 import ReactTooltip from "react-tooltip";
 import PropTypes from "prop-types";
 
-// const TOOLTIP_DEFINITIONS = [{
-//     id: 'element-id1',
-//     content: (<span>Tooltip1.</span>)
-// }, {
-//     id: 'element-id2',
-//     content: (<span>Tooltip2.</span>)
-// }];
-
 /**
  * Tooltips is a React tooltip component
  */
@@ -20,11 +12,6 @@ class Tooltips extends Component {
 
     render() {
         const {id, label, tooltip, fa} = this.props;
-        // for (const definition of TOOLTIP_DEFINITIONS) {
-        //     tooltips.push(<ReactTooltip key={definition.id} id={definition.id} className="summary-tooltips__tooltip"
-        //                                 place="right" type="dark" effect="solid" delayShow={300}><i
-        //         className="summary-tooltips__tooltip-icon fa fa-info-circle"/>{definition.content}</ReactTooltip>);
-        // }
 
         return (
             <span className="summary-tooltips">
