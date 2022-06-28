6813a422fa65fe41eecd1a1161312a1bf7b968fc
Ivan <ichang@broadinstitute.org>
2022-06-28 13:42:55 -0400

    Tweak cell styles.

--- test_runner_components/src/lib/components/Tables/IntegrationTestsTable.jsx
+++ test_runner_components/src/lib/components/Tables/IntegrationTestsTable.jsx
@@ -3,96 +3,114 @@ import React, {Component} from 'react';
 import Plot from 'react-plotly.js';
 import PropTypes from "prop-types";
 
-const cols = [
-    {title: 'Start time', field: 'startUserJourneyTimestamp', type: 'datetime'},
-    {title: 'Name', field: 'testScriptName'},
-    {title: 'Total', field: 'totalRun', type: 'numeric', hidden: true},
-    {title: 'Trend date', field: 'trend_date', hidden: true},
-    {title: 'Trend totalRun', field: 'trend_totalRun', hidden: true},
-    {title: 'Trend numCompleted', field: 'trend_numCompleted', hidden: true},
-    {title: 'Trend numExceptionsThrown', field: 'trend_numExceptionsThrown', hidden: true},
-    {title: 'Trend numExceptionsThrown neg', field: 'trend_numExceptionsThrown_neg', hidden: true},
-    {title: 'Outcome trend', field: 'trend',
-        render: (row) =>
-            <Plot
-                config={
-                    {
-                        displayModeBar: false,
-                    }
-                }
-                data={
-                    [
-                        {
-                            x: row.trend_date,
-                            y: row.trend_numCompleted,
-                            name: 'Passed',
-                            type: 'bar',
-                            width: 0.3,
-                            hovertemplate: '<i>%{x} Passed</i>: %{y}',
-                            marker: {
-                                color: 'rgba(50, 171, 96, 0.7)',
-                            },
-                        },
-                        {
-                            x: row.trend_date,
-                            y: row.trend_numExceptionsThrown_neg,
-                            text: row.trend_numExceptionsThrown,
-                            name: 'Failed',
-                            type: 'bar',
-                            width: 0.3,
-                            hovertemplate: '<i>%{x} Failed</i>: %{text}',
-                            marker: {
-                                color: 'rgba(219, 64, 82, 0.7)',
-                            }
-                        }
-                    ]
-                }
-                layout={ {
-                    width: 200,
-                    height: 120,
-                    barmode: 'relative',
-                    showlegend: false,
-                    margin: {
-                        l: 50,
-                        r: 50,
-                        b: 50,
-                        t: 50,
-                        pad: 4
-                    },
-                    xaxis: {
-                        visible: false,
-                        showticklabels: false,
-                    },
-                    yaxis: {
-                        visible: false,
-                        showticklabels: false,
-                    },
-                } }
-            />
-    },
-    {title: 'Failed', field: 'numExceptionsThrown', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric'},
-    {title: 'Passed', field: 'numCompleted', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric'},
-    {title: 'Skipped', field: 'numSkipped', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric',
-        render: (row) =>
-            row.totalRun - row.numCompleted - row.numExceptionsThrown
-    },
-];
-
 class IntegrationTestsTable extends Component {
     render() {
         const {data, title, barWidth} = this.props;
+        const cols = [
+            {title: 'Start time', field: 'startUserJourneyTimestamp', type: 'datetime'},
+            {title: 'Name', field: 'testScriptName'},
+            {title: 'Total', field: 'totalRun', type: 'numeric', hidden: true},
+            {title: 'Trend date', field: 'trend_date', hidden: true},
+            {title: 'Trend totalRun', field: 'trend_totalRun', hidden: true},
+            {title: 'Trend numCompleted', field: 'trend_numCompleted', hidden: true},
+            {title: 'Trend numExceptionsThrown', field: 'trend_numExceptionsThrown', hidden: true},
+            {title: 'Trend numExceptionsThrown neg', field: 'trend_numExceptionsThrown_neg', hidden: true},
+            {
+                title: 'Outcome trend', field: 'trend', sorting: false,
+                render: (row) =>
+                    <Plot
+                        config={
+                            {
+                                displayModeBar: false,
+                            }
+                        }
+                        data={
+                            [
+                                {
+                                    x: row.trend_date,
+                                    y: row.trend_numCompleted,
+                                    name: 'Passed',
+                                    type: 'bar',
+                                    width: 0.3,
+                                    hovertemplate: '<i>%{x} Passed</i>: %{y}',
+                                    marker: {
+                                        color: 'rgba(50, 171, 96, 0.7)',
+                                    },
+                                },
+                                {
+                                    x: row.trend_date,
+                                    y: row.trend_numExceptionsThrown_neg,
+                                    text: row.trend_numExceptionsThrown,
+                                    name: 'Failed',
+                                    type: 'bar',
+                                    width: 0.3,
+                                    hovertemplate: '<i>%{x} Failed</i>: %{text}',
+                                    marker: {
+                                        color: 'rgba(219, 64, 82, 0.7)',
+                                    }
+                                }
+                            ]
+                        }
+                        layout={{
+                            width: 200,
+                            height: 120,
+                            barmode: 'relative',
+                            showlegend: false,
+                            margin: {
+                                l: 50,
+                                r: 50,
+                                b: 50,
+                                t: 50,
+                                pad: 4
+                            },
+                            xaxis: {
+                                visible: false,
+                                showticklabels: false,
+                            },
+                            yaxis: {
+                                visible: false,
+                                showticklabels: false,
+                            },
+                        }}
+                    />
+            },
+            {
+                title: 'Failed', field: 'numExceptionsThrown',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric',
+                render: (row) =>
+                    row.numExceptionsThrown > 0
+                        ? <p style={{color: "rgba(219, 64, 82, 0.7)", fontWeight: "bold"}}>{row.numExceptionsThrown}</p>
+                        : <p>{row.numExceptionsThrown}</p>
+            },
+            {
+                title: 'Passed', field: 'numCompleted',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric',
+                render: (row) =>
+                    row.numCompleted > 0
+                        ? <p style={{color: "rgba(50, 171, 96, 0.7)", fontWeight: "bold"}}>{row.numCompleted}</p>
+                        : <p style={{color: "rgba(219, 64, 82, 0.7)", fontWeight: "bold"}}>{row.numCompleted}</p>
+            },
+            {
+                title: 'Skipped', field: 'numSkipped',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric',
+                render: (row) => {
+                    const skipped = row.totalRun - row.numCompleted - row.numExceptionsThrown;
+                    return skipped > 0
+                        ? <p style={{color: "rgba(219, 64, 82, 0.7)", fontWeight: "bold"}}>{row.numCompleted}</p>
+                        : <p>{skipped}</p>
+                },
+            },
+        ];
+
         return (
             <div>
                 <MaterialTable
@@ -103,7 +121,6 @@ class IntegrationTestsTable extends Component {
                         cellStyle: {
                             fontFamily: "'Lato', 'Helvetica Neue', Arial, sans-serif",
                             fontSize: "12px",
-                            textAlign: "left",
                             verticalAlign: "top",
                         },
                         headerStyle: {
--- test_runner_components/src/lib/components/Tables/PerfTestsTable.jsx
+++ test_runner_components/src/lib/components/Tables/PerfTestsTable.jsx
@@ -3,152 +3,165 @@ import React, {Component} from 'react';
 import Plot from 'react-plotly.js';
 import PropTypes from "prop-types";
 
-const cols = [
-    {title: 'Test time', field: 'startUserJourneyTimestamp', type: 'datetime'},
-    {title: 'Name', field: 'testScriptName', type: 'string'},
-    {title: 'Trend date', field: 'trend_date', hidden: true},
-    {title: 'Trend mean', field: 'trend_mean', hidden: true},
-    {title: 'Trend p50', field: 'trend_p50', hidden: true},
-    {title: 'Trend p95', field: 'trend_p95', hidden: true},
-    {title: 'Trend min', field: 'trend_min', hidden: true},
-    {title: 'Trend max', field: 'trend_max', hidden: true},
-    {title: 'Outcome trend', field: 'perf_trend',
-        render: (row) =>
-            <div><Plot
-                config={
-                    {
-                        displayModeBar: false,
-                    }
-                }
-                data={
-                    [
-                        {
-                            x: row.trend_date,
-                            y: row.trend_min,
-                            name: 'min [ms]',
-                            type: 'scatter',
-                            mode: 'lines',
-                            line: {
-                                width: 0,
-                            },
-                            showlegend: false,
-                        },
-                        {
-                            x: row.trend_date,
-                            y: row.trend_mean,
-                            name: 'mean [ms]',
-                            type: 'scatter',
-                            mode: 'lines',
-                            line: {
-                                color: 'rgba(63, 176, 198, 255)',
-                                dash: 'dash',
-                            },
-                            fill: 'tonexty',
-                            fillcolor: 'rgba(170, 218, 277, 255)',
-                            showlegend: false,
-                        },
-                        {
-                            x: row.trend_date,
-                            y: row.trend_p95,
-                            name: 'p95 [ms]',
-                            type: 'scatter',
-                            mode: 'lines',
-                            line: {
-                                width: 0,
-                            },
-                            fill: 'tonexty',
-                            fillcolor: 'rgba(130, 200, 213, 255)',
-                            showlegend: false,
-                        },
-                        {
-                            x: row.trend_date,
-                            y: row.trend_max,
-                            name: 'max [ms]',
-                            type: 'scatter',
-                            mode: 'lines',
-                            line: {
-                                width: 0,
-                            },
-                            fill: 'tonexty',
-                            fillcolor: 'rgba(170, 218, 277, 255)',
-                            showlegend: false,
-                        },
-                        {
-                            x: row.trend_date,
-                            y: row.trend_p50,
-                            name: 'p50 [ms]',
-                            type: 'scatter',
-                            mode: 'lines',
-                            line: {
-                                color: 'rgba(67, 178, 199, 255)',
-                            },
-                            showlegend: false,
-                        },
-                    ]
-                }
-                layout={ {
-                    width: 320,
-                    height: 240,
-                    legend: {
-                        font: {
-                            family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
-                            size: "9px",
-                        },
-                        yanchor: 'top',
-                        y: 1.01,
-                        xanchor: 'left',
-                        x: 0.1,
-                    },
-                    xaxis: {
-                        tickfont: {
-                            family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
-                            size: "9px",
-                        },
-                    },
-                    yaxis: {
-                        tickfont: {
-                            family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
-                            size: "9px",
-                        },
-                    },
-                } }
-            /></div>
-    },
-    {title: 'Min [ms]', field: 'min', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric'},
-    {title: 'Max [ms]', field: 'max', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric'},
-    {title: 'Mean [ms]', field: 'mean', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric'},
-    {title: 'Standard deviation [ms]', field: 'sd', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric'},
-    {title: 'p50 [ms]', field: 'p50', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric'},
-    {title: 'p95 [ms]', field: 'p95', cellStyle: {
-        textAlign: 'center', verticalAlign: "top",
-        }, headerStyle: {
-        textAlign: 'center',
-        }, type: 'numeric'},
-];
-
 class PerfTestsTable extends Component {
     render() {
         const {data, title} = this.props;
+        const cols = [
+            {title: 'Test time', field: 'startUserJourneyTimestamp', type: 'datetime'},
+            {title: 'Name', field: 'testScriptName', type: 'string'},
+            {title: 'Trend date', field: 'trend_date', hidden: true},
+            {title: 'Trend mean', field: 'trend_mean', hidden: true},
+            {title: 'Trend p50', field: 'trend_p50', hidden: true},
+            {title: 'Trend p95', field: 'trend_p95', hidden: true},
+            {title: 'Trend min', field: 'trend_min', hidden: true},
+            {title: 'Trend max', field: 'trend_max', hidden: true},
+            {
+                title: 'Outcome trend', field: 'perf_trend', sorting: false,
+                render: (row) =>
+                    <div><Plot
+                        config={
+                            {
+                                displayModeBar: false,
+                            }
+                        }
+                        data={
+                            [
+                                {
+                                    x: row.trend_date,
+                                    y: row.trend_min,
+                                    name: 'min [ms]',
+                                    type: 'scatter',
+                                    mode: 'lines',
+                                    line: {
+                                        width: 0,
+                                    },
+                                    showlegend: false,
+                                },
+                                {
+                                    x: row.trend_date,
+                                    y: row.trend_mean,
+                                    name: 'mean [ms]',
+                                    type: 'scatter',
+                                    mode: 'lines',
+                                    line: {
+                                        color: 'rgba(63, 176, 198, 255)',
+                                        dash: 'dash',
+                                    },
+                                    fill: 'tonexty',
+                                    fillcolor: 'rgba(170, 218, 277, 255)',
+                                    showlegend: false,
+                                },
+                                {
+                                    x: row.trend_date,
+                                    y: row.trend_p95,
+                                    name: 'p95 [ms]',
+                                    type: 'scatter',
+                                    mode: 'lines',
+                                    line: {
+                                        width: 0,
+                                    },
+                                    fill: 'tonexty',
+                                    fillcolor: 'rgba(130, 200, 213, 255)',
+                                    showlegend: false,
+                                },
+                                {
+                                    x: row.trend_date,
+                                    y: row.trend_max,
+                                    name: 'max [ms]',
+                                    type: 'scatter',
+                                    mode: 'lines',
+                                    line: {
+                                        width: 0,
+                                    },
+                                    fill: 'tonexty',
+                                    fillcolor: 'rgba(170, 218, 277, 255)',
+                                    showlegend: false,
+                                },
+                                {
+                                    x: row.trend_date,
+                                    y: row.trend_p50,
+                                    name: 'p50 [ms]',
+                                    type: 'scatter',
+                                    mode: 'lines',
+                                    line: {
+                                        color: 'rgba(67, 178, 199, 255)',
+                                    },
+                                    showlegend: false,
+                                },
+                            ]
+                        }
+                        layout={{
+                            width: 320,
+                            height: 240,
+                            legend: {
+                                font: {
+                                    family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                                    size: "9px",
+                                },
+                                yanchor: 'top',
+                                y: 1.01,
+                                xanchor: 'left',
+                                x: 0.1,
+                            },
+                            xaxis: {
+                                tickfont: {
+                                    family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                                    size: "9px",
+                                },
+                            },
+                            yaxis: {
+                                tickfont: {
+                                    family: "'Lato', 'Helvetica Neue', Arial, sans-serif",
+                                    size: "9px",
+                                },
+                            },
+                        }}
+                    /></div>
+            },
+            {
+                title: 'Min [ms]', field: 'min',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric'
+            },
+            {
+                title: 'Max [ms]', field: 'max',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric'
+            },
+            {
+                title: 'Mean [ms]', field: 'mean',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric'
+            },
+            {
+                title: 'Standard deviation [ms]', field: 'sd',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric'
+            },
+            {
+                title: 'p50 [ms]', field: 'p50',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric'
+            },
+            {
+                title: 'p95 [ms]', field: 'p95',
+                cellStyle: {
+                    paddingRight: '40px',
+                    verticalAlign: 'top',
+                }, type: 'numeric'
+            },
+        ];
+
         return (
             <div>
                 <MaterialTable
@@ -159,7 +172,6 @@ class PerfTestsTable extends Component {
                         cellStyle: {
                             fontFamily: "'Lato', 'Helvetica Neue', Arial, sans-serif",
                             fontSize: "12px",
-                            textAlign: "left",
                             verticalAlign: "top",
                         },
                         headerStyle: {
